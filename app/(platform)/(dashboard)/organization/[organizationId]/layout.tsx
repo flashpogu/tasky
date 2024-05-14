086f584ca4aad947a3a93306c5ec002390d6fdaa
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-05-14 18:48:07 +0530

    single board page impemented

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import Org_Control from "./_components/org_Control";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 export default function OrganizationIdLayout({
   children,
